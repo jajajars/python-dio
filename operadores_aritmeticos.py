969ecabd4c622ed390e85fe185bc9d40e38886a5
Jajars <jailson.marques91@gmail.com>
2024-06-21 22:14:36 -0300

    adicionado operadores_aritmeticos.py

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,21 @@
+print("Operações básicas:")
+print(1 + 1) #adição
+print(10 - 2) #subtração
+print(10 * 3) #multiplicação
+print(12 / 3 ) #divisão (retorna float)
+print(12 // 3) #divisão inteiros (retorna int)
+print(10 % 3) #módulo
+print(10 ** 2) #exponenciação
+
+'''Ordem de operações
+   - Parênteses
+   - Exponenciações
+   - Multiplicações e Divisões
+   - Adições e Subtrações'''
+
+print("Ordem de operações")
+print(10 - 5 * 2)
+print((10 - 5) * 2)
+print(10 ** 2 * 2)
+print(10 ** (2 * 2))
+print(10 / 2 * 4)
