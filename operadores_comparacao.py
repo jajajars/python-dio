df73acfc0ce231f5911d3d76dbd1831829d37bcc
Jajars <jailson.marques91@gmail.com>
2024-06-23 19:04:59 -0300

    Adicionado arquivo operadores_comparacao.py

---
+++ operadores_comparacao.py
@@ -0,0 +1,9 @@
+saldo = 450
+saque = 200
+
+print(saldo == saque) #igualdade
+print(saldo != saque) #diferença
+print(saldo > saque) #comparação maior que
+print(saldo >= saque) #comparação maiou ou igual a
+print(saldo < saque) #comparação menor que
+print(saldo <= saque) #comparação menor ou igual a
