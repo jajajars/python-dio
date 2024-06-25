321f570eebd18d62a3be4bd26c70f3e074b17ccc
Jajars <jailson.marques91@gmail.com>
2024-06-25 18:00:01 -0300

    adicionado identacao_blocos.py

---
+++ identacao_blocos.py
@@ -0,0 +1,11 @@
+def sacar(valor):
+    saldo = 500
+    if saldo >= valor:
+        print("Valor sacado")
+    print("Obrigado por utilizar")
+
+def depositar(valor):
+    saldo = 500
+    saldo += valor
+
+sacar(100)
