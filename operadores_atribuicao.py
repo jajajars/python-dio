a9a1743baf8878f18780a4cf0901f1563fc70e45
Jajars <jailson.marques91@gmail.com>
2024-06-25 15:23:13 -0300

    Adicionado arquivo operadores_atribuicao.py

---
+++ operadores_atribuicao.py
@@ -0,0 +1,24 @@
+saldo = 500 #Atribuição simples
+print(saldo)
+
+saldo += 200 #atribuição com adição
+print(saldo)
+
+saldo -= 100 #atribuição com subtração
+print(saldo)
+
+saldo *= 2 #atribuição com multiplicação
+print(saldo)
+
+saldo /= 3 #atribuição com divisão (float)
+print(saldo)
+
+saldo = 400
+saldo //= 2 #atribuição com divisão (int)
+print(saldo)
+
+saldo %= 3 #atribuição com módulo. Retorna o módulo da variável no número atribuído
+print(saldo) #saldo = 200. 200 mod 3 = 2
+
+saldo **= 4 #atribuição com exponenciação
+print(saldo)
