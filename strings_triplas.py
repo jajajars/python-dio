2df0d91dbc035d17f6c06e736b1da9e89e143630
Jajars <jailson.marques91@gmail.com>
2024-06-26 14:26:37 -0300

    adicionado strings_triplas.py

---
+++ strings_triplas.py
@@ -0,0 +1,19 @@
+nome = "Jajars"
+
+print(f"""
+    Olá, meu nome é {nome}
+        Eu estou apredendo Python
+        """)
+
+# para adicionar strings de múltiplas linhas é necessário utilizar aspas simples ou duplas 3 vezes para abertura e fechamento da string. Todos os espaços são considerados. 
+
+menu = """
+====== MENU ======
+
+1. Depósito
+2. Saque
+3. Encerrar
+
+==================
+"""
+print(menu)
